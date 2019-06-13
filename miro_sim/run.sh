71b50ad8225e4256dc6ef790f223c1520c6dd33b
David B <dbuxton@gmail.com>
2019-06-13 12:01:45 +0100

    MiRo simulator tidying

--- miro_sim/run.sh
+++ miro_sim/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Run ROS Core in the background and Gazebo in foreground
+# Run a ROS core in the background and Gazebo in foreground
 roscore \
 & gazebo /root/mdk/sim/worlds/miro.world --verbose \
 #& /root/mdk/share/python/miro2/core/client_demo.py - caml \
 #& /root/mdk/share/python/miro2/core/client_demo.py - camr \
-#/root/mdk/share/python/miro2/core/client_demo.py
-echo "Gazebo will sometimes fail with a BadDrawable error; just keep retrying until it works!"
+#& /root/mdk/share/python/miro2/core/client_demo.py
+echo "If Gazebo has failed with a BadDrawable error, just keep retrying until it works!"
