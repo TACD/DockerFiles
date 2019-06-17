6bf98c569d2296de078435278d1352f1b673c549
David B <dbuxton@gmail.com>
2019-06-17 17:48:28 +0100

    MDK

--- mdk/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run a ROS core in the background
-roscore 
-#& /root/mdk/share/python/miro2/core/client_demo.py - caml \
-#& /root/mdk/share/python/miro2/core/client_demo.py - camr \
-#& /root/mdk/share/python/miro2/core/client_demo.py
